89c942f959f6374c1e535eaeb3cc4014410acd3f
Avging <2178881296@qq.com>
2023-10-14 20:25:29 +0800

    在Java中操作Redis_Spring Data Redis_Hash类型

--- sky-server/src/test/java/com/avging/test/SpringDataRedisTest.java
+++ sky-server/src/test/java/com/avging/test/SpringDataRedisTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -33,5 +36,30 @@ public class SpringDataRedisTest {
         redisTemplate.opsForValue().set("code","1234",3, TimeUnit.MINUTES);
         redisTemplate.opsForValue().setIfAbsent("lock","1");
         redisTemplate.opsForValue().setIfAbsent("lock","2");
+    }
+
+
+    /**
+     * 操作哈希类型的数据
+     */
+    @Test
+    public void testHash(){
+        //hset hget hdel hkeys hvals
+        HashOperations hashOperations = redisTemplate.opsForHash();
+
+        hashOperations.put("100","name","tom");
+        hashOperations.put("100","age","20");
+
+        String name = (String) hashOperations.get("100", "name");
+        System.out.println(name);
+
+        Set keys = hashOperations.keys("100");
+        System.out.println(keys);
+
+        List values = hashOperations.values("100");
+        System.out.println(values);
+
+        hashOperations.delete("100","age");
+
     }
 }
