dc633c812437f256cc1eb4b1094d56c46306e673
Avging <2178881296@qq.com>
2023-10-16 21:19:37 +0800

    HttpClient_发送POST方式请求

--- sky-server/src/test/java/com/avging/test/HttpClientTest.java
+++ sky-server/src/test/java/com/avging/test/HttpClientTest.java
@@ -3,9 +3,12 @@ package com.avging.test;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -45,4 +48,42 @@ public class HttpClientTest {
     /**
      * 测试通过httpclient 发送 PUT方式的请求
      */
+    @Test
+    public void testPOST() throws Exception{
+        //创建httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        //创建请求对象
+        HttpPost httpPost = new HttpPost("http://localhost:8080/admin/employee/login");
+
+        //传参： JSON数据
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username","admin");
+        jsonObject.put("password","123456");
+
+        //StringEntity 是HttpEntity接口的实现类
+        StringEntity entity = new StringEntity(jsonObject.toString());
+        //指定请求编码方式
+        entity.setContentEncoding("utf-8");
+        //数据格式
+        entity.setContentType("application/json");
+        httpPost.setEntity(entity);
+
+        //发送请求
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+
+
+        //解析返回结果
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("响应码为：" + statusCode);
+
+        HttpEntity entity1 = response.getEntity();
+        String body = EntityUtils.toString(entity1);
+        System.out.println("响应数据为：" + body);
+
+        //关闭资源
+        response.close();;
+        httpClient.close();;
+    }
+
 }
