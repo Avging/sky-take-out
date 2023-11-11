2c78b25ee7c7ad614e759d1417c58e60f062173c
Avging <2178881296@qq.com>
2023-11-11 19:13:59 +0800

    查看购物车

--- sky-server/src/main/java/com/avging/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/avging/controller/user/ShoppingCartController.java
@@ -36,14 +36,14 @@ public class ShoppingCartController {
 
     /**
      * 查看购物车
-     * @return Result
+     * @return Result<List<ShoppingCart>>
      */
-//    @GetMapping("/list")
-//    @ApiOperation("查看购物车")
-//    public Result<List<ShoppingCart>> list(){
-//        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
-//        return Result.success(list);
-//    }
+    @GetMapping("/list")
+    @ApiOperation("查看购物车")
+    public Result<List<ShoppingCart>> list(){
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+        return Result.success(list);
+    }
 
     /**
      * 清空购物车
--- sky-server/src/main/java/com/avging/controller/user/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/avging/controller/user/ShoppingCartServiceImpl.java
@@ -82,15 +82,15 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
      * 查看购物车
      * @return
      */
-//    public List<ShoppingCart> showShoppingCart() {
-//        //获取到当前微信用户的id
-////        Long userId = BaseContext.getCurrentId();
-////        ShoppingCart shoppingCart = ShoppingCart.builder()
-////                .userId(userId)
-////                .build();
-////        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
-////        return list;
-//    }
+    public List<ShoppingCart> showShoppingCart() {
+        //获取到当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 
     /**
      * 清空购物车
